4a47ede9f6702b0249236cdb7c70e7565ad1eea8
uhuruskylogistics-beep <uhuruskylogistics@gmail.com>
2026-06-09 08:03:08 +0100

    Update Next.js config for GitHub Pages static export

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: '',
+  assetPrefix: '',
+};
+
+module.exports = nextConfig;
